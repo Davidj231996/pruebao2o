b8a538de53852df780ca5c3ae5151a35f96ffe95
Davidj231996 <alerodiez8dj@hotmail.com>
2023-10-04 09:49:06 +0200

    Se ha añadido Behat para los test funcionales

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
